a1f3e1619b0cbe49ad03ddfed936a467b00d0c97
Bayan2019 <saparbayevabt@gmail.com>
2025-03-29 14:43:06 +0500

    migrateUp

--- repositories/sql/schema/002_insert_users.sql
+++ repositories/sql/schema/002_insert_users.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-INSERT INTO users(name, password_hash, iin, phone, date_of_birth)
+INSERT INTO users(name, password_hash, iin, phone)
     VALUES ('admin', '$2a$10$vqjSWa5BZEvN/ef7a5pTTOZLImNdubTNVqmyU.7ctiG3kEXyrGk/C', '000000000000', '+0(000)000-00-00');
 
 -- +goose Down
